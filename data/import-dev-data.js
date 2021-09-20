04f88fab91b5c71b0ed921fad8ac336a9356dbe0
Silas Santos da Silva <silvasilas66@gmail.com>
2021-09-20 20:24:25 -0300

    Making seeder file to create and delete data from colection

---
+++ data/import-dev-data.js
@@ -0,0 +1,32 @@
+const { error } = require('console');
+const fs = require('fs');
+const databaseConnect = require('./../config/database')();
+const Tour = require("./../models/Tour");
+
+// Read JSON file
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import data into DataBase
+const importData = async () => {
+    try {
+        await Tour.create(tours);
+        console.log('Data created with success');
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+// Delete all data from DataBase
+const deleteData = async () => {
+    try {
+        await Tour.deleteMany();
+        console.log('Data deleted successfully');
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+if (process.argv[2] === '--import') importData()
+else if (process.argv[2] === '--delete') deleteData()
