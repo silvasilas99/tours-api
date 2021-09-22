5251e1b7c44e8324c7263de596aa6b417f621d55
Silas Santos da Silva <silvasilas66@gmail.com>
2021-09-22 19:19:15 -0300

    Creating method and route to advanced filtered tours

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -15,3 +15,22 @@ exports.getSimpleFilteredTours = async (req, res) => {
         return res.send(err.message || "An error as occurred. Try again later");
     }
 }
+
+exports.getAdvancedFilteredTours = async (req, res) => {
+    try {
+        const queryObj = {...req.query};
+        const excludedFields = ['page', 'sort', 'count', 'limit', 'fields'];
+        excludedFields.forEach (element => delete queryObj[element]);
+
+        let queryStr = JSON.stringify (queryObj);
+        queryStr = queryStr.replace (/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        const tours = await Tour.find(JSON.parse(queryStr));
+        return res.status(200).json({
+            results: tours.length,
+            data: tours
+        }); 
+    } catch (err) {
+        return res.send(err.message || "An error as occurred. Try again later");
+    }
+}
--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const tourController = require('./../controllers/tour.controller');
 
 router.get('/simpleFilter', tourController.getSimpleFilteredTours);
+router.get('/advancedFilter', tourController.getAdvancedFilteredTours);
 
 module.exports = router;
