70cbfcd598e0e5553dc376ac8061f6359c197565
Silas Santos da Silva <silvasilas66@gmail.com>
2021-09-22 19:31:18 -0300

    Unifying routes and methods

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -1,22 +1,6 @@
 const Tour = require('./../models/Tour');
 
-exports.getSimpleFilteredTours = async (req, res) => {
-    try {
-        const queryObj = {...req.query};
-        const excludedFields = ['page', 'sort', 'count', 'limit', 'fields'];
-        excludedFields.forEach(element => delete queryObj[element]);
-
-        const tours = await Tour.find(queryObj);
-        return res.status(200).json({
-            results: tours.length,
-            data: tours
-        }); 
-    } catch (err) {
-        return res.send(err.message || "An error as occurred. Try again later");
-    }
-}
-
-exports.getAdvancedFilteredTours = async (req, res) => {
+exports.getTours = async (req, res) => {
     try {
         const queryObj = {...req.query};
         const excludedFields = ['page', 'sort', 'count', 'limit', 'fields'];
--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const tourController = require('./../controllers/tour.controller');
 
-router.get('/simpleFilter', tourController.getSimpleFilteredTours);
-router.get('/advancedFilter', tourController.getAdvancedFilteredTours);
+router.get('/getTours', tourController.getTours);
 
 module.exports = router;
