f921b1b2a1e0b4fb03934225b7b9615e76ddcaa3
Silas Santos da Silva <silvasilas66@gmail.com>
2021-09-24 14:17:06 -0300

    Adding feature to select fields to getTours method

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -2,17 +2,30 @@ const Tour = require('./../models/Tour');
 
 exports.getTours = async (req, res) => {
     try {
+        // Allowing reserved verbs
         const queryObj = {...req.query};
         const excludedFields = ['page', 'sort', 'count', 'limit', 'fields'];
         excludedFields.forEach (element => delete queryObj[element]);
 
+        // Using advanced filtering with main operators
         let queryStr = JSON.stringify (queryObj);
         queryStr = queryStr.replace (/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
         let query = Tour.find(JSON.parse(queryStr));
 
+        // Sorting 
         if (req.query.sort) {
             const sortBy = req.query.sort.split(',').join(' ');
             query = query.sort(sortBy);
+        } else {
+            query = query.sort('createdAt');
+        }
+
+        // Selecting fields
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v');
         }
 
         const tours = await query;
