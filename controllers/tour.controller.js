09fd3136d41ec45ecdf9ae0f78438f75c649aac8
Silas Santos da Silva <silvasilas66@gmail.com>
2021-11-18 18:00:40 -0300

    Refatoring getTours controller

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -1,44 +1,20 @@
 const Tour = require('./../models/Tour');
+const BasicFeatures = require("../utils/basicFeatures");
 
 exports.getTours = async (req, res) => {
     try {
-        // Allowing reserved verbs
-        const queryObj = {...req.query};
-        const excludedFields = ['page', 'sort', 'count', 'limit', 'fields'];
-        excludedFields.forEach (element => delete queryObj[element]);
+        const features = new BasicFeatures(Tour.find(), req.query)
+            .filter()
+            .sort()
+            .limitFields()
+            .paginate();
 
-        // Using advanced filtering with main operators
-        let queryStr = JSON.stringify (queryObj);
-        queryStr = queryStr.replace (/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-        let query = Tour.find(JSON.parse(queryStr));
-
-        // Sorting 
-        if (req.query.sort) {
-            const sortBy = req.query.sort.split(',').join(' ');
-            query = query.sort(sortBy);
-        } else {
-            query = query.sort('createdAt');
-        }
-
-        // Selecting fields
-        if (req.query.fields) {
-            const fields = req.query.fields.split(',').join(' ');
-            query = query.select(fields);
-        } else {
-            query = query.select('-__v');
-        }
-
-        // Pagination
-        const count =+ req.query.count || 100;
-        const page =+ req.query.page || 1;
-        query = query.skip(count * (page - 1)).limit(count);
-        
-        const tours = await query;
+        const tours = await features.query;
 
         return res.status(200).json({
             results: tours.length,
             data: tours
-        }); 
+        });
     } catch (err) {
         return res.send(err.message || "An error as occurred. Try again later");
     }
